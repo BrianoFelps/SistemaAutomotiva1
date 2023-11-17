9b96b1bbd86c6c72e8bbc59be29129fe0a7eab41
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2023-11-17 17:28:59 -0300

    OrdemServicoMeio

--- src/main/java/com/mycompany/dao/DaoOrdemServico.java
+++ src/main/java/com/mycompany/dao/DaoOrdemServico.java
@@ -39,4 +39,29 @@ public class DaoOrdemServico extends BancoDeDadosMySQL{
             return false;
         }
     }
+    
+    public Boolean alterar (int id, int nidEmp, int nidVeic, int nidCliente, int nidProdSer, int nidFuncionario, String nobs, Date nFaturaIn, Date nFaturaFim){
+    try{
+        sql = "UPDATE ORDEM_DE_SERVICO SET IDEMPRESA = ?, IDVEICULO = ?, IDCLIENTE = ?, IDPRODSER = ?, IDFUNCIONARIO = ?, OBSERVACAO = ?, FATURAIN = ?, FATURAFIM = ? WHERE ID= ?";
+        
+        setStatement(getConexao().prepareStatement(sql));
+        
+        getStatement().setInt(9, id);
+        getStatement().setInt(1, nidEmp);
+        getStatement().setInt(2, nidVeic);
+        getStatement().setInt(3, nidCliente);
+        getStatement().setInt(4, nidProdSer);
+        getStatement().setInt(5, nidFuncionario);
+        getStatement().setString(6, nobs);
+        getStatement().setDate(7, nFaturaIn);
+        getStatement().setDate(8, nFaturaIn);
+
+            getStatement().executeUpdate();
+            
+            return true;
+    }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+    }
+    }
 }
